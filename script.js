c6851b67ff5e4b71b8482517d73e3954a8e6d0fc
LoooMeN <looomen@hotmail.com>
2024-07-23 08:28:29 +0300

    adapt

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
     let accordions = document.querySelectorAll('.faq-item');
     let burger = document.querySelector('#burger');
-    let header = document.querySelector('.header')
+    let header = document.querySelector('.header-wrapper')
+    let mobileMenu = document.querySelector('.mobile-menu');
 
     accordions.forEach((acc) => {
         acc.addEventListener('click', () => {
@@ -10,6 +11,16 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 
     burger.addEventListener('click', () => {
-        header.classList.toggle('--open');
+        if (!header.classList.contains('--open')) {
+            mobileMenu.style.display = "block";
+            setTimeout(() => {
+                header.classList.toggle('--open');
+            }, 0)
+        } else {
+            header.classList.toggle('--open');
+            setTimeout(() => {
+                mobileMenu.style.display = "none";
+            }, 300)
+        }
     })
 })
