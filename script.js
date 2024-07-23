3ef5d145a717262cd9dc6f91d5582ef977ef0fa7
looomen <looomen@hotmail.com>
2024-07-23 06:35:57 +0300

    12123

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
     let accordions = document.querySelectorAll('.faq-item');
+    let burger = document.querySelector('#burger');
+    let header = document.querySelector('.header')
 
     accordions.forEach((acc) => {
         acc.addEventListener('click', () => {
-            acc.classList.toggle('--open')
+            acc.classList.toggle('--open');
         })
     });
+
+    burger.addEventListener('click', () => {
+        header.classList.toggle('--open');
+    })
 })
